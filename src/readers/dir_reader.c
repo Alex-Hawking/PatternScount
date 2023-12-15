9389a8a5261a3663541476b18a053af3efd322de
Alex Hawking <alexhawking23@gmail.com>
2023-12-15 17:07:26 +0800

    implement forking and recusrive search (does not work efficently)

--- src/readers/dir_reader.c
+++ src/readers/dir_reader.c
@@ -5,6 +5,7 @@ void open_dir(char *path, char *regex_pattern) {
     struct dirent *dir;
     struct stat fileStat;
     char fullPath[1024]; 
+    pid_t pid;
 
     d = opendir(path);
     if (d == NULL) {
@@ -24,15 +25,27 @@ void open_dir(char *path, char *regex_pattern) {
             snprintf(fullPath, sizeof(fullPath), "%s/%s", path, dir->d_name);
         }
 
-        if (stat(fullPath, &fileStat) < 0) {
-            perror("stat");
+        if (lstat(fullPath, &fileStat) < 0) {
+            perror("lstat");
             continue;
         }
 
         if (S_ISREG(fileStat.st_mode)) {
-            match_lines_file(fullPath, regex_pattern);
-        } else if (S_ISDIR(fileStat.st_mode)) {
-            printf("Directory: %s\n", fullPath);
+            pid = fork();
+            if (pid == -1) {
+                perror("fork");
+                continue;
+            }
+
+            if (pid == 0) { 
+                match_lines_file(fullPath, regex_pattern);
+                exit(0);
+            } else { 
+                int status;
+                waitpid(pid, &status, 0); 
+            }
+        } else if (S_ISDIR(fileStat.st_mode) && !S_ISLNK(fileStat.st_mode)) {
+            open_dir(fullPath, regex_pattern);
         }
     }
 
